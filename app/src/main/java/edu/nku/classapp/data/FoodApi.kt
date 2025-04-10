99abf885b96ab340467d6585be94c92d27e3c9b4
Abby Jones <145145964+jonesa122@users.noreply.github.com>
2025-04-09 20:12:30 -0400

    Change URL

--- app/src/main/java/edu/nku/classapp/data/FoodApi.kt
+++ app/src/main/java/edu/nku/classapp/data/FoodApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface FoodApi {
-    @GET("/api/v2/product/{barcode}")
+    @GET("/product/{barcode}")
     suspend fun getProduct(@Path("barcode") barcode: Int): Response<FoodResponse>
 }
